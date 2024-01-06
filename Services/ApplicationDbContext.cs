716401ad833ce47e23e344b3c8f0ceb205540e60
Walid Abd El Azeem <walid.abdelazeem@gmail.com>
2024-01-06 17:19:19 +0200

    Add files via upload

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product_Tutorial.Models;
+
+namespace Product_Tutorial.Services
+{
+    public class ApplicationDbContext:DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions) : base(contextOptions)
+        { }
+        public DbSet<ProductTable> productTables { get; set;  }
+    }
+}
