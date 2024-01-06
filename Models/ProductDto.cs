fa76add376aec81f13ab22a08f848c25954fa7ed
Walid Abd El Azeem <walid.abdelazeem@gmail.com>
2024-01-06 17:10:40 +0200

    Add files via upload

---
+++ Models/ProductDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Tutorial.Models
+{
+    public class ProductDto
+    {
+
+
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+        public string? Description { get; set; }
+
+        public IFormFile? ImageFileName { get; set; }
+    }
+}
---
+++ Models/ProductTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Product_Tutorial.Models
+{
+    public class ProductTable
+    {
+    
+
+
+
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = "";
+
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
